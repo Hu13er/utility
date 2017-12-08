5922df7ec27f2ef7108483f4b80f596d984b1e6f
Ali Esmaili Seraji <faramin.smile@gmail.com>
2017-12-08 16:23:52 +0330

    Add some i3 configs.

---
+++ kbd-backlight.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# Keyboard Backlight
+# 
+
+import sys
+
+MAGIC_FILE_ADDR = "/sys/class/leds/smc::kbd_backlight/brightness"
+
+def get_keyboard_backlight():
+    with open(MAGIC_FILE_ADDR, 'r') as f:
+        return int(f.read().strip())
+
+def set_keyboard_backlight(value):
+    with open(MAGIC_FILE_ADDR, 'w') as f:
+        f.write(value)
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print(get_keyboard_backlight())
+        sys.exit(0)
+
+    if sys.argv[1] == 'inc':
+        value = 10 if len(sys.argv) <= 2 else int(sys.argv[2])
+        set_keyboard_backlight(get_keyboard_backlight() + value)
+    elif sys.argv[1] == 'dec':
+        value = 10 if len(sys.argv) <= 2 else int(sys.argv[2])
+        set_keyboard_backlight(get_keyboard_backlight() - value)
+    elif sys.argv[1] == 'get':
+        print(get_keyboard_backlight())
+    elif sys.argv[1] == 'set':
+        set_keyboard_backlight(sys.argv[2])
+
